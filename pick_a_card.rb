43eba293d9387e24549f04d9e8ebf56fba5a1bec
danikdesign <danik.newmail@gmail.com>
2022-04-19 16:06:14 +0300

    Add a unicode symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 #comment for test
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['Diamonds♦', 'Hearts♥', 'Clubs♣', 'Spades♠']
 puts "#{values.sample} of #{suits.sample}"
